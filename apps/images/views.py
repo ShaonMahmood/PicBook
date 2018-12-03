4e996c28a6823c8f0d06bc20af97050f98a133ea
Mahmood1342 <mahmood.habib.cuet@gmail.com>
2018-12-03 15:49:24 +0600

    creating a custom decorator for ajax request check

--- apps/images/views.py
+++ apps/images/views.py
@@ -6,6 +6,7 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from .models import Image
 from .forms import ImageCreateForm
+from apps.common.decorators import ajax_required
 
 @login_required
 def image_create(request):
@@ -42,7 +43,7 @@ def image_detail(request, id, slug):
 
 
 
-
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
