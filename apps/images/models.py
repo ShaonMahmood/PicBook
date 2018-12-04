620eed94960fe832c28334d4bddeaacac4702912
Mahmood1342 <mahmood.habib.cuet@gmail.com>
2018-12-04 13:17:08 +0600

    using django signals to count total likes

--- apps/images/models.py
+++ apps/images/models.py
@@ -14,6 +14,8 @@ class Image(models.Model):
     url = models.URLField()
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
+    total_likes = models.PositiveIntegerField(db_index=True,
+                                              default=0)
     created = models.DateField(auto_now_add=True,
                                db_index=True)
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
